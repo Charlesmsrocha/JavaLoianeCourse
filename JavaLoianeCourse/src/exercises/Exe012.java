181a8ee079ef358d35e76796ccc77c4a275a7601
Charles Rocha <charlesmsrocha@gmail.com>
2022-06-14 19:12:22 +0100

    Exercise 12 - Average weight

---
+++ JavaLoianeCourse/src/exercises/Exe012.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exe012 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Obtaining the height data of a person, write an algorithm that calculates the ideal weight, using the formula (72.7*height) - 58.");
+		System.out.println("----------------------------------------------------------------------------------------------------------------------------------");
+
+		Scanner scankb = new Scanner(System.in);
+		
+		System.out.println("Type the person height: ");
+		float height = scankb.nextFloat();
+		
+		double weight = (height*72.7) - 58;
+		
+		System.out.println("This person average weight might be: " + weight + ".");
+
+	}
+
+}
