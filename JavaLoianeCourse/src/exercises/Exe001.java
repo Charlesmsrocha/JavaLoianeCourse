8bdb7bec38774d3cccee6acf9693004be23b7fcf
Charles Rocha <charlesmsrocha@gmail.com>
2022-06-14 15:02:15 +0100

    Exercise number 1

---
+++ JavaLoianeCourse/src/exercises/Exe001.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class Exe001 {
+
+	public static void main(String[] args) {
+		System.out.println("Write software that shows 'Hello World' on the screen. ");
+		System.out.println("--------------------------------------------------------");
+		System.out.println("Hello World!");
+
+	}
+
+}
