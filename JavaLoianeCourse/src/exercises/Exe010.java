90c7210de9fc531a4e4c799b541bf816e81bce58
Charles Rocha <charlesmsrocha@gmail.com>
2022-06-14 17:31:37 +0100

    Exercise 10 - Farenheit to Celsius.

---
+++ JavaLoianeCourse/src/exercises/Exe010.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exe010 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Write a software that asks temperature in Celsius, \n"
+				+ "and then shows it in Farenheit degrees. ");
+		System.out.println("--------------------------------------------------------");
+
+		Scanner scankb = new Scanner(System.in);
+		
+		System.out.print("Type how many Celsius degrees: ");
+		int celsius = scankb.nextInt();
+		
+		double farenheit = (celsius *1.8) + 32;
+		
+		System.out.print(celsius + " Celsius degrees is equal to " + farenheit + " Farenheit degrees.");
+	}
+
+}
