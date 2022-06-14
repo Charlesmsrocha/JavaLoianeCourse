4e90524732205c6146e3443eca257c7214b5601f
Charles Rocha <charlesmsrocha@gmail.com>
2022-06-14 14:51:59 +0100

    Testing commit on Eclipse

---
+++ JavaLoianeCourse/src/com/loiane/cursojava/aula10/Variables.java
@@ -0,0 +1,34 @@
+package com.loiane.cursojava.aula10;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		//Convenção Java
+		int idade;
+		String nome;
+		String nomeDoMeuCachorro = "Toto";
+		
+		//aceito, mas não utilizado
+		int _idade;
+		int $idade;
+		
+		//não é convenção Java
+		String ano2014;
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomedoMeuCachorro;
+		
+		idade=25;
+		
+		System.out.println("Idade = " + idade);		
+		
+		// má prática
+		
+		int a = 10;
+		String b = "Loiane";	
+
+	}
+
+}
