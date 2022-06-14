2f0f8dda6083b0e62028d519c21770767a18a368
Charles Rocha <charlesmsrocha@gmail.com>
2022-06-14 17:00:23 +0100

    Exercise 5

---
+++ JavaLoianeCourse/src/exercises/Exe006.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exe006 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Write software that asks for a circle radius, then calculate and show its area.");
+		System.out.println("-------------------------------------------------------------------------------");
+		
+		Scanner scankb = new Scanner(System.in);
+		
+		System.out.print("Type the circle radius: ");
+		float radius = scankb.nextFloat();
+		
+		float pi = 3.1415f;
+		float area = pi * (radius*radius);
+		
+		System.out.print("The circle area is " + area);
+
+	}
+
+}
