1959e88b7e30c44a8acc86381f58ad1e8237c3ca
Charles Rocha <59806026+Charlesmsrocha@users.noreply.github.com>
2022-06-14 14:15:21 +0100

    First commit.
    Files added by upload.
    First time doing this.

---
+++ JavaLoianeCourse/src/com/loiane/cursojava/aula10/Variablesint.java
@@ -0,0 +1,23 @@
+package com.loiane.cursojava.aula10;
+
+public class Variablesint {
+
+	public static void main(String[] args) {
+		
+		byte idade01 = 20;
+		
+		short idade02 = 21;
+		
+		int idade03 = 22;
+		
+		long idade04 = 23;
+		
+
+		
+		System.out.println("Valor variável 01 = " + idade01);
+		System.out.println("Valor variável 02 = " + idade02);
+		System.out.println("Valor variável 03 = " + idade03);
+		System.out.println("Valor variável 04 = " + idade04);
+	}
+
+}
